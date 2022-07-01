5b263141390b5585bb253d82478c6ed25defcd0a
yusuf591-cpu <yusufsaydullayev@gmail.com>
2022-07-01 18:59:55 +0500

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, Suspense } from "react";
+import { useDispatch } from "react-redux";
+import { fetchTodos } from "./redux/addtodo_reducer";
+// import AddTodoForm from './components/AddTodoForm';
+// import TodoList from './components/TodoList';
+// import TotalCompleteItems from './components/TotalCompleteItems';
 
-function App() {
+const AddTodoForm = React.lazy(() => import("./components/AddTodoForm"));
+const TodoList = React.lazy(() => import("./components/TodoList"));
+const TotalCompleteItems = React.lazy(() =>import("./components/TotalCompleteItems"));
+
+const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchTodos());
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container bg-white p-4 mt-5">
+      <h1>Todo List</h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <AddTodoForm />
+        <TodoList />
+        <TotalCompleteItems />
+      </Suspense>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+// import { useQuery } from 'react-query';
+import { useDispatch,useSelector } from 'react-redux'
+// import { addTodo } from '../redux/addtodo_reducer';
+
+const TodoList = () => {
+
+	const dispatch = useDispatch();
+
+	// const  data  = useQuery('repoData', () =>
+	// fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
+	// .then(res => res.json())
+	// .then(data => dispatch(addTodo(data))))
+	// console.log('query',data)
+
+	const { status, error, todos } = useSelector((state) => state.todos) 
+
+	return (
+
+		<ul className='list-group'>
+			{status === 'loading' && <h2>Loading...</h2>}
+			{error && <h2>Something went wrong:{error}</h2>}
+			{todos.map((todo) => (
+				<TodoItem id={todo.id} title={todo.title} completed={todo.completed} key={todo.id} />
+			))}
+		</ul>
+	);
+};
+
+export default TodoList;
